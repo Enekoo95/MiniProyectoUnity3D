54d2c05cf5e8bd6d182267f122551fe158aa8476
Enekoo95 <eneko_martin@euneiz.com>
2025-04-03 19:36:21 +0200

    Health Recover + Heart UI Implementation

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,14 +1,18 @@
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int maxHealth = 100;
+    public int maxHealth = 50;
     public int currentHealth;
+
+    // Array de corazones
+    public GameObject[] hearts;
 
     void Start()
     {
         currentHealth = maxHealth;
+        UpdateHearts();
     }
 
     public void TakeDamage(int damage)
@@ -16,9 +20,21 @@ public class PlayerHealth : MonoBehaviour
         currentHealth -= damage;
         Debug.Log("El jugador recibió " + damage + " de daño. Vida restante: " + currentHealth);
 
+        UpdateHearts();
+
         if (currentHealth <= 0)
         {
             Die();
+        }
+    }
+
+    void UpdateHearts()
+    {
+        int heartCount = Mathf.CeilToInt((float)currentHealth / (maxHealth / hearts.Length));
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            hearts[i].SetActive(i < heartCount);
         }
     }
 
@@ -26,20 +42,28 @@ public class PlayerHealth : MonoBehaviour
     {
         Debug.Log("¡El jugador ha muerto!");
 
-        // Desactivar el movimiento del jugador
         GetComponent<PlayerController>().enabled = false;
         GetComponent<CharacterController>().enabled = false;
 
-        // Opcional: Desactivar el objeto del jugador
         gameObject.SetActive(false);
 
-        // Opcional: Reiniciar el nivel después de 2 segundos
         Invoke("RestartLevel", 2f);
     }
 
     void RestartLevel()
     {
-        // Recargar la escena actual
-        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+
+        // Limitar la salud al máximo
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+
+        Debug.Log("El jugador ha sanado " + amount + " puntos de vida. Vida actual: " + currentHealth);
+        UpdateHearts();
     }
 }
