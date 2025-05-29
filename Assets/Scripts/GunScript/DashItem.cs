39b4e0880ebfa0a2e8d95494b7371428c23e4ba0
Enekoo95 <eneko_martin@euneiz.com>
2025-05-29 11:12:54 +0200

    Sword bug fixed

--- Assets/Scripts/GunScript/DashItem.cs
+++ Assets/Scripts/GunScript/DashItem.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class DashItem : MonoBehaviour
 {
+    public Transform dashPosition;
     private void OnTriggerEnter(Collider other)
     {
         PlayerDash playerDash = other.GetComponent<PlayerDash>();
@@ -13,6 +14,20 @@ public class DashItem : MonoBehaviour
             playerDash.SetSwordEquipped(true);
 
             Debug.Log("Espada recogida y equipada");
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("¡Has recogido la espada!");
+
+            // Hacer que la espada sea hija del jugador y se coloque en la posición correcta
+            transform.SetParent(dashPosition);
+            transform.localPosition = new Vector3(0, 0.05f, 0.15f);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+            // Desactivar el collider para que no vuelva a recogerse
+            GetComponent<Collider>().enabled = false;
+
         }
     }
 }
